81829e5717d83b539ffbeff23aecafde726aefb8
Aleks XXIIV <143701547+AleksDen5@users.noreply.github.com>
2024-03-03 10:52:40 +0100

    Create Register.vue

---
+++ dog-images-website/src/components/Register.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Register</h1>
+    <form @submit.prevent="register">
+      <input v-model="username" placeholder="Username" />
+      <input v-model="password" type="password" placeholder="Password" />
+      <button type="submit">Register</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import api from '@/api.js';
+
+export default {
+  name: 'Register',
+  data() {
+    return
