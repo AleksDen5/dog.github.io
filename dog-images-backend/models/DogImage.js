ddbec39a8b1857322725e727edf3f9f152a7ea30
Aleks XXIIV <143701547+AleksDen5@users.noreply.github.com>
2024-03-03 10:51:51 +0100

    Create DogImage.js

---
+++ dog-images-backend/models/DogImage.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const DogImageSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  url: { type: String, required: true },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+});
+
+module.exports = mongoose.model('DogImage', DogImageSchema);
